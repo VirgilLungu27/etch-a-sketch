4339aa4478e4cd02a3731eec94a4ebb35c9ddfcd
Virgil <lunguvirgil3000@gmail.com>
2022-06-13 11:23:30 +0300

    Fix popAlert function where rows and cols were defined as objects and not numbers

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 let cell; 
+let newCell;
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
@@ -10,14 +11,24 @@ function makeRows(rows, cols) {
     container.appendChild(cell).className = "grid-item";
   };
 };
-let startValue = makeRows(16, 16);
 
+ function replaceRows(rows, cols) {
+   container.innerHTML = '';
+   container.style.setProperty('--grid-rows', rows);
+   container.style.setProperty('--grid-cols', cols);
+   for (let c = 0; c < (rows * cols); c++) {
+     newCell = document.createElement("div");
+     newCell.innerText = (c + 1);
+     container.appendChild(newCell).className = "grid-item";
+  }
+};
 
 const button = document.getElementById("pop");
 
-pop.onclick = function popAlert(rows, cols) {   
-   prompt("Enter number of rows:", rows);
-   prompt("Enter number of columns:", cols);
-   container.innerHTML = '';
-   };
+pop.onclick = function popAlert() {   
+  let rows = prompt("Enter number of rows:");
+  let cols = prompt("Enter number of columns:");
+  replaceRows(rows, cols);
+};
 
+let startValue = makeRows(16, 16);
