ab4bb821b90f27cdd2ea879973148b547496ac1c
Virgil <lunguvirgil3000@gmail.com>
2022-06-13 10:23:14 +0300

    Clear field once button is pressed

--- java.js
+++ java.js
@@ -1,21 +1,23 @@
 const container = document.getElementById("container");
+let cell; 
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
+  for (let c = 0; c < (rows * cols); c++) {
+    cell = document.createElement("div");
     cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
+let startValue = makeRows(16, 16);
 
-makeRows(16, 16);
 
 const button = document.getElementById("pop");
 
-pop.onclick = function popAlert(rows, cols) {
-   let newRow = prompt("Enter number of rows:", rows);
-   let newColumn = prompt("Enter number of columns:", cols);
-   makeRows(newRow, newColumn);
-}
+pop.onclick = function popAlert(rows, cols) {   
+   prompt("Enter number of rows:", rows);
+   prompt("Enter number of columns:", cols);
+   container.innerHTML = '';
+   };
+
